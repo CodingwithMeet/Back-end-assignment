0bc235dbfd252cb608790ede6a91c25a5d659202
CodingwithMeet <155745700+CodingwithMeet@users.noreply.github.com>
2024-01-19 17:19:10 +0530

    Add files via upload

---
+++ GreenStich_Backend_Assignment-main/GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/jwt/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.GreenStitch.jwt;
+
+public interface SecurityConstants {
+
+	public static final String JWT_KEY ="secretmeet$77$Soni~meetu448~";
+	public static final String JWT_HEADER ="Authorization";
+}
+
